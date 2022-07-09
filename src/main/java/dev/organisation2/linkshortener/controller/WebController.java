53f6a71c84794dce4878893ddd62b9a176608f15
Simon <n3ck11@gmail.com>
2022-07-09 15:31:38 +0200

    add WebController

---
+++ src/main/java/dev/organisation2/linkshortener/controller/WebController.java
@@ -0,0 +1,12 @@
+package dev.organisation2.linkshortener.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class WebController {
+    @RequestMapping("/")
+    String indexHtml() {
+        return "index.html";
+    }
+}
