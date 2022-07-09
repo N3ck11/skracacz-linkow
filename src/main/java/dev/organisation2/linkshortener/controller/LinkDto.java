5d04c96a522d93fd53df457807fccf6402f1fae8
Simon <n3ck11@gmail.com>
2022-07-09 15:57:22 +0200

    update LinkDto

--- src/main/java/dev/organisation2/linkshortener/controller/LinkDto.java
+++ src/main/java/dev/organisation2/linkshortener/controller/LinkDto.java
@@ -1,12 +1,30 @@
 package dev.organisation2.linkshortener.controller;
 
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.time.LocalDate;
 
 public class LinkDto {
 
 
-    public LinkDto(String id, String email, int visits, String targetUrl, LocalDate expirationDate, int i) {
+    String id; String email; String targetUrl; LocalDate expirationDate; int i;
+
+
+    public LinkDto(String id, String email, String targetUrl, LocalDate expirationDate, int i) {
+        this.id = id;
+        this.email = email;
+        this.targetUrl = targetUrl;
+        this.expirationDate = expirationDate;
+
+
     }
 
+    public String getShortenedLink() {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/s/{id}")
+                .buildAndExpand(id)
+                .toUriString();
+    }
 }
